21d1853d056f4cf7cea35973386a71f1ffce33a3
Josh <yr18478@bristol.ac.uk>
2021-02-20 12:19:07 +0000

    Implemented solve_to

--- ode_solver.py
+++ ode_solver.py
@@ -10,3 +10,18 @@ def euler_step(func: Callable[[float, float], float], x0: float, t0:float, h: fl
 	x1 = x0 + dydx * h
 
 	return x1
+
+def solve_to(step_func: Callable, f: Callable[[float, float], float], x0: float, t0: float, t1: float, hmax: float) -> float:
+	'''
+		Performs integration steps using step function on f from t0 to t1 in steps no larger than hmax
+	'''
+	t = t0
+	x = x0
+	while t + hmax < t1:
+		x = step_func(f, x, t, hmax)
+		t += hmax
+
+	# Do final step to t1
+	x = step_func(f, x, t, t1 - t)
+
+	return x
