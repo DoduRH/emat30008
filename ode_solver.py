89cb028a01e75127257be6940a049433c4c5bc2c
Josh <yr18478@bristol.ac.uk>
2021-02-20 15:07:15 +0000

    Added solve_ode function

--- ode_solver.py
+++ ode_solver.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, List
 
 
 def euler_step(func, x0, t0, h):
@@ -27,3 +27,37 @@ def solve_to(step_func, f, x0, t0, t1, hmax):
 	x = step_func(f, x, t, t1 - t)
 
 	return x
+
+def solve_ode(f, x0, t, hmax, method="euler"):
+	# type: (Callable, float, List[float], float, str) -> List[float]
+	'''
+		generates a series of numerical solution estimates for f at points in t
+	'''
+
+	# get step function from method string
+	method = method.lower()
+	if method in methods.keys():
+		# Get step function
+		step_func = methods[method]
+	else:
+		# If string is not a recognised method
+		raise ValueError(f"method must be one of {list(methods.keys())}")
+
+	# Set up intitial variables
+	x_out = []
+	t_start = t[0]
+	x = x0
+
+	# Solve between values t_end and t_start
+	for t_end in t:
+		x = solve_to(step_func, f, x, t_start, t_end, hmax)
+		x_out.append(x)
+		# Update start range
+		t_start = t_end
+
+	return x_out
+
+# Dictionary of available integration methods
+methods = {
+	"euler": euler_step,
+}
