0736a59a3e9622d9f8444876c2d556825e33eda7
Josh <yr18478@bristol.ac.uk>
2021-03-08 00:27:43 +0000

    Updated ode_poerformance for system of odes

--- ode_performance.py
+++ ode_performance.py
@@ -3,7 +3,6 @@
 from timer import timer
 from ode_solver import solve_ode
 from math import nan, exp
-from numba import njit
 
 # %%
 # Set variables
@@ -18,18 +17,11 @@ h_values = {
 # %%
 # Run and measure execution time for each method
 for method, h in h_values.items():
-    # Setup func as a function that can be compiled
-    f = njit(func)
-
-    # Run solve_ode once to compile functions
-    res = solve_ode(f, x0, t, h, method)
-
     # Time it
     with timer() as clock:
         # Run 100 times to reduce variance
-        for i in range(100):
-            res = solve_ode(f, x0, t, h, method)
+        res = solve_ode(func, x0, t, h, method)
         # Get elapsed time
         time_spent = clock.elapse
         # Print error and execution time
-        print(f"Error for {method} is {abs(res[-1] - analytic_sol(t[-1], nan))} and took {time_spent} seconds with a step size of {h}")
+        print(f"Error for {method} is {abs(res[-1][0] - analytic_sol(t[-1], nan))} and took {time_spent} seconds with a step size of {h}")
