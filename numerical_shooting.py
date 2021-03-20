42653fb6a3dc1ee28aa9988f570c8338b6d79062
Josh <yr18478@bristol.ac.uk>
2021-03-20 17:59:05 +0000

    Numerical shooting for any ODE system

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -1,32 +1,72 @@
 # %%
 import numpy as np
 from ode_solver import solve_ode
+from repeat_finder import find_repeats
 from scipy.optimize import fsolve
 
+def shoot(f, initial, tmax=200):
+    """Use numerical shooting to calculate the initial conditions and period for f
+
+    Args:
+        f (list): system of ODEs as a list
+        initial (list): Initial conditions to start from
+        tmax (int, optional): Max value to get initial period guess. Defaults to 200.
+
+    Returns:
+        list: List containing initial conditions and period of the function
+    """
+
+    # Find approximate location of periodic behaviour
+    rk4_solution = solve_ode(f, initial, np.linspace(0, tmax, 500), 0.1, "rk4")
+    initial_conditions, period = find_repeats(rk4_solution, abs_tol=0.08)
+
+    # Setup g
+    g = lambda U: [
+        *(U[:-1] - solve_ode(f, U[:-1], [0, U[-1]], 0.1, "RK4")[-1]),
+        f[0](U[-1], *U[:-1]), # dx/dt(0) = 0
+    ]
+
+    # Find roots of g
+    # TODO: Option to use custom root finder
+    orbit = fsolve(g, [*initial_conditions, period])
+
+    # Make sure only singular orbit was found by dividing the period 
+    # period until it no longer gives g(orbit) approx= 0
+    Gx = 0
+    divisor = 2
+    
+    # This assumes the minimum period is 1
+    while divisor < orbit[-1]: 
+        while np.allclose(Gx, 0, atol=1e-3):
+            orbit[-1] /= divisor
+            Gx = g(orbit)
+        divisor += 1
+        
+    # Run fsolve once more to re-align to a single orbit
+    # TODO: Option to use custom root finder
+    orbit = fsolve(g, orbit)
+
+    return orbit
 # %%
 if __name__=="__main__":
-    #G(U0) = u0 - F(u0,T)
+    # Setup equations
+    # Lokta Volterra variables
     alpha = 1
     delta = 0.1
     beta = 0.2
 
-    U = np.array([
-        0.25,
-        0.25,
-    ])
-
+    # Lokta-Volterra equations
     funcs = [
-        lambda t, x, y: x * (1 - x) - (alpha * x * y) / (delta + x), # x
-        lambda t, x, y: beta * y * (1 - (y/x)), # y
+        lambda t, x, y: x * (1 - x) - (alpha * x * y) / (delta + x), # dx/dt
+        lambda t, x, y: beta * y * (1 - (y/x)), # dy/dt
     ]
 
-    g = lambda u: u - solve_ode(funcs, u, [0,10], 0.1, "RK4")[-1]
+    # Lokta-Voltera initial conditions
+    initial = [
+        0.25,
+        0.25,
+    ]
 
-    guess = [0.5, 0.5]
-
-    res = fsolve(g, guess)
-    print(res)
-    print(g(res))
-    print(np.isclose(g(res), [0,0]))
+    print(f'{shoot(funcs, initial)=}')
 
 # %%
