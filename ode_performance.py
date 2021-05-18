6bac1513a5a2b8d70a6d26eec5757f03eab486aa
Josh <yr18478@bristol.ac.uk>
2021-05-18 23:56:07 +0100

    Set time ODE performance tests

--- ode_performance.py
+++ ode_performance.py
@@ -1,6 +1,6 @@
 # %%
 # Import libraries
-from timer import timer
+import time
 from ode_solver import solve_ode
 from math import nan, exp
 
@@ -14,14 +14,25 @@ h_values = {
     "euler": 1e-6,
     "rk4": 1e-1
 }
+
+#h_values = {
+#    "euler": 0.1,
+#    "rk4": 0.1
+#}
 # %%
 # Run and measure execution time for each method
+# Number of seconds to run for
+num_seconds = 30
+
 for method, h in h_values.items():
     # Time it
-    with timer() as clock:
-        # Run 100 times to reduce variance
+    # Run for 30 seconds to reduce variance
+    start_time = time.time()
+    total = 0
+    while time.time() - start_time < num_seconds:
         res = solve_ode(func, x0, t, h, method)
-        # Get elapsed time
-        time_spent = clock.elapse
-        # Print error and execution time
-        print(f"Error for {method} is {abs(res[-1][0] - analytic_sol(t[-1], nan))} and took {time_spent} seconds with a step size of {h}")
+        total += 1
+    end_time = time.time()
+    # Print error and execution time
+    print(f"Error for {method} is {abs(res[-1][0] - analytic_sol(t[-1], nan))} with a step size of {h}")
+    print(f"and ran {total} itterations in {round(end_time - start_time, 3)} seconds giving {round(total/(end_time-start_time), 3)} iterations/second\n")
