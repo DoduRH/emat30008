eaeec2b3cc7d016ecd7c33eeafe18c8d7f0d0daf
Josh <yr18478@bristol.ac.uk>
2021-05-19 00:14:10 +0100

    Added argument checks to measure_performance

--- measure_performance.py
+++ measure_performance.py
@@ -1,23 +1,34 @@
 # Import libraries
 import time
+import numpy as np
 
 def perf_measure(func, num_seconds, *args):
     """Measure the performance of a function
 
     Args:
-        func (function): Function to measure the performance of
+        func (function): Function to measure the performance of.  Called as ``func(*args)``
         num_seconds (float): Number of seconds to measure the performance over
-        args: Arguments to call func with
 
     Returns:
         tuple: (total time, number of iterations, result of 1 call)
     """
 
+    # Check number of seconds is valid
+    # Check it can be a float
+    if not np.can_cast(num_seconds, float):
+        raise TypeError("num_seconds must be castable to float")
+    else:
+        # Convert num_seconds to float
+        num_seconds = np.float(num_seconds)
+
+    # Check it is greater than 0
     if num_seconds <= 0:
         raise ValueError("num_seconds must be larger than 0")
 
+    # Initialise variables
     total_iterations = 0
     start_time = time.time()
+    # Run the function loop
     while time.time() - start_time < num_seconds:
         res = func(*args)
         total_iterations += 1
@@ -29,5 +40,12 @@ def perf_measure(func, num_seconds, *args):
 
 if __name__ == "__main__":
     # Test on sleep function
+
+    # Test with no args
+    for i in [1, 4]:
+        print(perf_measure(lambda: time.sleep(i), 10))
+    
+    # Test with args
     for i in [1, 4]:
         print(perf_measure(time.sleep, 10, i))
+
