3b46c8db9fc350850a6eac5043ba24cdbef1c70b
Josh <yr18478@bristol.ac.uk>
2021-05-22 13:06:05 +0100

    Improved periodic detection and parameter checks in repeat finder

--- repeat_finder.py
+++ repeat_finder.py
@@ -4,78 +4,114 @@ import numpy as np
 class TimePeriodNotFoundError(Exception):
     pass
 
-def find_repeats(arr):
+def find_repeats(arr, closeArgs=None):
     """Gets estimate for repeated values in arr
 
     Args:
         arr (ndarray): Array of data to search through
+        closeArgs (dict, optinal): Arguments to pass to np.allclose to determine if 2 points are close enough.  Defaults to None which expands to rtol=0.05, atol=0.01
 
     Returns:
         tuple: tuple of (0, 0, ..., -1) if search fails or (x, y, ..., period)
     """
-    # TODO: Clean inputs prior to use
+
+    # arr must be floats
+    if not np.can_cast(arr, float):
+        raise TypeError("arr must be castable to floats")
+
+    # closeArgs parameter
+    if closeArgs is None:
+        closeArgs = dict(rtol=0.05, atol=0.01)
+    elif type(closeArgs) != dict:
+        raise TypeError("closeArgs must be a dictionary with keys 'rtol' and/or 'atol'")
 
     # Reshape arr to be n by x array of numbers
-    arr = np.reshape(arr, (len(arr), -1))
+    arr = np.reshape(arr, (len(arr), -1)).astype(float)
 
+    # Round to 3DP
     rounded = np.around(arr[:,0], 3)
 
+    # Get unique values
     unique = np.array(np.unique(rounded, axis=0, return_counts=True, return_index=True)).T
 
-    # Ignore y vals and count
+    # Get index of unique values
+    # Ignore y vals
     _, index, count = unique[unique[:,2].argsort()][-1]
 
     output = arr[int(index)]
-
+    
+    # If half the points are the same, it is an equilibrium (period of 0)
     if count > len(arr)*0.5:
-        # If half the points are the same, it is an equilibrium (period of 0)
         return (*output, 0)
-
+    
+    # Has the current point gone outside of allclose range
+    within_range = False
     occurences = []
-    last = np.nan
     for i, row in enumerate(arr):
-        # TODO: Allclose tolerances as arguments
-        if np.allclose(row, output, rtol=0.05, atol=0.01):
-            # If last added was not within the last 5 itterations append it
-            if last < i - 5:
+        if np.allclose(row, output, **closeArgs):
+            # If it has been outside 'close' range
+            if not within_range:
                 occurences.append(i)
-            last = i
+                within_range = True
+        else:
+            within_range = False
 
-    occ_array = np.array(occurences)
-
+    # Must be at least 3 occurences to be considered periodic
     if len(occurences) < 3:
         return (*(0,)*len(output), -1)
 
+    occ_array = np.array(occurences)
+
+    # Difference in occurences
     dif = occ_array[1:] - occ_array[:-1]
 
+    # Average of differences
     period = np.mean(dif)
 
     return (*output, period)
 
 
-def find_period(func, t0=1, tstep=10, tmax=np.inf, params=[]):
+def find_period(func, t0=1, tstep=10, tmax=np.inf, *args):
     """Find estimate for period and initial conditions of func
 
     Args:
-        func (function): Function to find period of.  Must take a single argument of t as an array
+        func (function): Function to find period of.  Must take a single argument of t as an array. `func(t, *args)`
         t0 (int, optional): Starting value for T, must be positive. Defaults to 1.
         tstep (float, optional): Steps of t0 to take. Defaults to 1.
         tmax (float, optional): Max value for T. Defaults to np.inf.
+        args: Passed to func
 
     Returns:
         tuple: tuple of (0, 0, ..., -1) if search fails or (x, y, ..., period)
     """
-    # TODO: Clean inputs prior to use
+    # Clean inputs prior to use
+    # t0
+    if not np.can_cast(t0, float):
+        raise TypeError("t0 must be castable to a float")
+    else:
+        t0 = float(t0)
+
+    # tstep
+    if not np.can_cast(tstep, float):
+        raise TypeError("tstep must be castable to a float")
+    else:
+        tstep = float(tstep)
+
+    # tmax
+    if not np.can_cast(tmax, float):
+        raise TypeError("tmax must be castable to a float")
+    else:
+        tmax = float(tmax)
 
     t = np.arange(0, t0, 0.1)
-    # TODO: Cache solution and append new variables to increase performance?
+
     # Would require func to take t and x0
-    *initials, period = find_repeats(func(t, *params))
+    *initials, period = find_repeats(func(t, *args))
 
     while period == -1 and t0 < tmax:
         t0 += tstep
         t = np.arange(0, t0, 0.1)
-        *initials, period = find_repeats(func(t, *params))
+        *initials, period = find_repeats(func(t, *args))
 
     # If no period was found, raise error
     if period == -1:
@@ -91,9 +127,9 @@ if __name__ == "__main__":
     delta = 0.1
     beta = 0.2
 
-    Lokta_Volterra = [
-        lambda t, x, y: x * (1 - x) - (alpha * x * y) / (delta + x), # dx/dt
-        lambda t, x, y: beta * y * (1 - (y/x)), # dy/dt
+    Lokta_Volterra = lambda t, U: [
+        U[0] * (1 - U[0]) - (alpha * U[0] * U[1]) / (delta + U[0]), # dx/dt
+        beta * U[1] * (1 - (U[1]/U[0])), # dy/dt
     ]
 
     print(find_period(lambda t: solve_ode(Lokta_Volterra, [0.25, 0.25], t, 0.1, "rk4")))
