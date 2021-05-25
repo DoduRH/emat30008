c82e7b90ced4ad01f8f9ed638561cac13310a73a
Josh <yr18478@bristol.ac.uk>
2021-05-25 20:25:12 +0100

    Pseudo-arclength continuation working

--- numerical_continuation.py
+++ numerical_continuation.py
@@ -2,7 +2,7 @@ from numerical_shooting import shoot
 from root_finder import find_root
 import numpy as np
 
-def continuation(func, x0, par0, vary_par, par_max, delta_multiplier=0.1, discretisation=lambda x: x, solver=find_root):
+def continuation(func, x0, par0, vary_par, par_max, discretisation=lambda x: x, solver=find_root):
     """Finds points where the function is stable
 
     Args:
@@ -11,7 +11,6 @@ def continuation(func, x0, par0, vary_par, par_max, delta_multiplier=0.1, discre
         par0 (Dict): Initial parameters
         vary_par (str): Key of dictionary to vary
         par_max (float): Max value of var_par
-        delta_multiplier (float, optional): Jump between points. Defaults to 0.1.
         discretisation (function, optional): Discretisation function. Defaults to passing func through.
         solver (function, optional): Solver to use. Defaults to find_root.
 
@@ -20,86 +19,85 @@ def continuation(func, x0, par0, vary_par, par_max, delta_multiplier=0.1, discre
     """
 
     output = []
-    last_result = np.append(x0, par0[vary_par]) # x0.copy()
-    old_result = last_result[:]
-    while last_result[-1] < par_max:
-        old_old_result = old_result[:]
-        old_result = last_result[:]
+    v = []
+    v.append(np.append(x0, par0[vary_par]))
+    v.append(np.array((*solver(discretisation(func), v[0][:-1], args=({vary_par: v[0][-1] + 0.1})), v[0][-1] + 0.1)))
+    import matplotlib.pyplot as plt
+    while v[-1][-1] < par_max:
+        secant = v[-1] - v[-2]
 
-        # Secant
-        u_delta = old_result - old_old_result
+        predict = v[-1] + secant
 
-        # Prediction
-        prediction = last_result + u_delta * delta_multiplier
-
-        # NOTE: 0.1 shouldnt be needed
-        g = lambda U: [
-            *discretisation(func)(U[:-1], p={vary_par: U[-1]}),
-            0.1-np.dot(prediction - U, last_result - U),  # delta v dot (v - v_tilde)
+        g = lambda Vn, p: [
+            *discretisation(func)(Vn[:-1], {vary_par:  Vn[-1]}),
+            np.dot(Vn - predict, secant),
         ]
 
-        last_result = solver(g, prediction)
+        v.append(solver(g, predict, args=({vary_par: predict[-1]})))
 
-        print(f"Done {last_result=} {func(last_result[:-1], p={vary_par: last_result[-1]})=}")
-        output.append((last_result[-1], *last_result[:-1]))
+        print(f"Found root {v[-1]} which gives {g(v[-1], {vary_par: predict[-1]})}")
+
+        xs = predict[1], v[-1][1]
+        ys = predict[0], v[-1][0]
+        plt.plot(xs, ys)
+        #print(f"Done {v[-1]=} {func(v[-1][:-1], p={vary_par: v[-1][-1]})=}")
+        output.append((v[-1][-1], *v[-1][:-1]))
     
-    par0[vary_par] = par_max
-    last_result = fsolve(discretisation(func), last_result[:-1], args=par0)
-    output.append((par0[vary_par], *last_result))
-    print(f"Done {last_result=} {func(last_result, p=par0)=}")
+    #par0[vary_par] = par_max
+    #v.append(np.array((*solver(discretisation(func), v[0][:-1], args=({vary_par: v[0][-1] + 0.1})), par_max)))
+    #print(f"Done {v[-1]=} {func(v[-1][:-1], p={vary_par: v[-1][-1]})=}")
+    #output.append((v[-1][-1], *v[-1][:-1]))
 
     return output
 
 if __name__ == "__main__":
-    par0 = dict(
-        beta = 0
-    )
-
     x0 = [0.2, 0.2]
 
     # Hopf Bifurcation
-    theODE = lambda t, U, p: [
+    hopf = lambda t, U, p: [
                 p['beta'] * U[0] - U[1] - U[0] * (U[0] ** 2 + U[1] ** 2),
         U[0] +  p['beta'] * U[1] -        U[1] * (U[0] ** 2 + U[1] ** 2),
     ]
 
-    par0 = dict(
-        alpha = 1,
-        delta = 0.1,
-        beta = 0.2,
+    hopfPar = dict(
+        beta = -1,
     )
 
-    def Lokta_Volterra(t, U, p):
-        x = U[0] * (1 - U[0]) - (p['alpha'] * U[0] * U[1]) / (p['delta'] + U[0]) # dx/dt
-        y = p['beta'] * U[1] * (1 - (U[1]/U[0])) # dy/dt
-
-        return [x, y]
-
-    # Lokta-Voltera initial conditions
-    initial = [
+    hopfInitial = [
+        1.5,
         1.5,
     ]
 
     from scipy.optimize import fsolve
 
-    par0 = dict(c=-2)
-    theODE = lambda U, p: [
+    cubicInitial = [1.5]
+    cubicPar = dict(c=-2)
+    cubic = lambda U, p: [
         U[0] ** 3 - U[0] + p['c']
     ]
 
-    #par0 = dict(alpha=0)
-    #theODE = lambda t, r, p: p['alpha'] * r + r ** 3 - r ** 5
-
+    # Continuation on the cubic equation
     results = continuation(
-        theODE,  # the ODE to use
-        initial,  # the initial state
-        par0,  # the initial parameters
+        cubic,  # the equation to use
+        cubicInitial,  # the initial state
+        cubicPar,  # the initial parameters
         vary_par="c",  # the parameter to vary
         par_max=2,
-        delta_multiplier=0.1, # max step size
         discretisation=lambda x: x,  # the discretisation to use
         solver=fsolve,  # the solver to use
     )
+
+    # Continuation on the hopf bifurcation equation
+    # results = continuation(
+    #     hopf,  # the ODE to use
+    #     hopfInitial,  # the initial state
+    #     hopfPar,  # the initial parameters
+    #     vary_par="beta",  # the parameter to vary
+    #     par_max=2,
+    #     delta_multiplier=0.1, # max step size
+    #     discretisation=lambda f: lambda g, p: shoot(f=f, initial=g, approximate_solution=g, ODEparams=[p]),  # the discretisation to use
+    #     solver=fsolve,  # the solver to use
+    # )
     
     import matplotlib.pyplot as plt
     import numpy as np
@@ -113,5 +111,6 @@ if __name__ == "__main__":
     plt.title("Graph showing numerical continuation on $y=x^3 - x + c$")
 
     plt.plot(x, ys)
+    plt.scatter(x, ys)
 
     plt.show()
