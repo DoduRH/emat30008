3e8e1b025463df2e61bf146d529110f6302c88b4
Josh <yr18478@bristol.ac.uk>
2021-05-27 19:41:31 +0100

    Removed multiple period check and fixed docstring

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -4,12 +4,13 @@ from ode_solver import solve_ode
 from repeat_finder import TimePeriodNotFoundError, find_period
 from root_finder import find_root
 
-def shoot(f, initial, approximate_solution=None, tmax=np.inf, solver=find_root, ODEparams=[]):
+def shoot(f, initial, approximate_period=None, tmax=np.inf, solver=find_root, ODEparams=None):
     """Use numerical shooting to calculate the initial conditions and period for f
 
     Args:
-        ODE (function): ODE, must return a list.  Is called as ODE(t, U, *ODEparams)
+        f (function): f, must return a list.  Is called as `f(t, U, *ODEparams)`
         initial (list): Initial conditions to start from
+        approximate_period (float): Approximate period. Defaults to None in which case `find_period` will be used to estimate
         tmax (int, optional): Max value to get initial period guess. Defaults to 200.
         ODEparams (list, optional): Optional extra parameters to pass to the ODE. Defaults to []
 
@@ -17,16 +18,20 @@ def shoot(f, initial, approximate_solution=None, tmax=np.inf, solver=find_root, 
         list: List containing initial conditions and period of the function
     """
 
+    if ODEparams is None:
+        ODEparams = []
+
     # Find approximate location of periodic behaviour
-    if approximate_solution is None:
+    if approximate_period is None:
         approx_period = find_period(lambda t: solve_ode(f, initial, t, 0.1, "rk4", ODEparams), tmax=tmax)
+        if approx_period[-1] == -1:
+            raise TimePeriodNotFoundError
     else:
-        approx_period = np.append(initial, approximate_solution)
+        approx_period = np.append(initial, approximate_period)
+    
+    if approx_period[-1] < 0:
+        raise ValueError(f"Invalid time period {approx_period[-1]}")
 
-    if approx_period[-1] == -1:
-        raise TimePeriodNotFoundError
-
-    # NOTE: Is f[0](U[-1], *U[:-1]) a general phase condition?
     # Setup g
     g = lambda U: [
         *(U[:-1] - solve_ode(f, U[:-1], [0, U[-1]], 0.1, "RK4", ODEparams)[-1]),
@@ -35,24 +40,6 @@ def shoot(f, initial, approximate_solution=None, tmax=np.inf, solver=find_root, 
 
     # Find roots of g
     orbit = solver(g, approx_period)
-
-    # Make sure only singular orbit was found by dividing the period 
-    # period until it no longer gives g(orbit) approx= 0
-    x0 = orbit[:-1]
-    divisor = 2
-    
-    # This assumes the minimum period is 1
-    if approximate_solution is None:
-        while divisor < orbit[-1]: 
-            while np.allclose(orbit[:-1], x0, rtol=1e-3):
-                orbit[-1] /= divisor
-                x0 = solve_ode(f, orbit[:-1], [0, orbit[-1]], 0.1, "RK4", ODEparams)[-1]
-            orbit[-1] *= divisor # revert final division
-            x0 = orbit[:-1] # Reset x0
-            divisor += 1
-        
-    # Run solver once more to re-align to a single orbit
-    orbit = solver(g, orbit)
 
     return orbit
 # %%
